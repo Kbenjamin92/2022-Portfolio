43a9ba86cb82818d9fb07a5481c48fdb6235a073
Kbenjamin92 <kipplbenjamin@gmail.com>
2022-06-23 09:41:26 -0400

    added header content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import staticContent from '../static/assets.json'
 
 const Header = () => {
+
+  const headerContent = staticContent.map((asset, keys) => {
+    return (
+        <div key={keys}>
+            <h1>{asset.HEADER.title}</h1>
+        </div>
+      );
+  });
+
   return (
-    <div></div>
+    <>{headerContent}</>
   )};
 
 export default Header;
