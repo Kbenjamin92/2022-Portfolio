abea6c8da5e0fb1714356a5a2ecc41e40b1b3371
Kbenjamin92 <kipplbenjamin@gmail.com>
2022-06-24 17:33:37 -0400

    working on about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,18 +7,21 @@ const About = () => {
   const about = aboutContent.map((asset, keys) => {
     return (
       <div key={keys}>
-        <h2 className="about-title">{asset.MAIN.ABOUT.title}</h2>
+        <h2 className="about-main-title">{asset.MAIN.ABOUT.title}</h2>
+        <div className="about-title-border"></div>
         <div className="about-container">
           <div>
-            <h3>{asset.MAIN.ABOUT.experience.title}</h3>
+            <h3 className="about-title">{asset.MAIN.ABOUT.experience.title}</h3>
             <p className="about-description">{asset.MAIN.ABOUT.experience.description}</p>
           </div>
+          <div className="experience-border"></div>
           <div>
-            <h3>{asset.MAIN.ABOUT.personality.title}</h3>
+            <h3 className="about-title">{asset.MAIN.ABOUT.personality.title}</h3>
             <p className="about-description">{asset.MAIN.ABOUT.personality.description}</p>
           </div>
+          <div className="personality-border"></div>
           <div>
-            <h3>{asset.MAIN.ABOUT.goals.title}</h3>
+            <h3 className="about-title">{asset.MAIN.ABOUT.goals.title}</h3>
             <p className="about-description">{asset.MAIN.ABOUT.goals.description}</p>
           </div>
         </div>
@@ -27,8 +30,8 @@ const About = () => {
   });
 
   return (
-    <div>
-    {about}
+    <div className="container">
+      {about}
     </div>
   )};
 
