bcc2e29bd4a4cec8aaaaa180d9cc86300a811e55
Kbenjamin92 <kipplbenjamin@gmail.com>
2022-06-25 09:50:44 -0400

    added some icons to the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import '../styles/About.css';
 import aboutContent from '../static/assets.json';
+import person from '../icons/person-icon.svg';
+import smile from '../icons/smile-icon.svg';
+import check from '../icons/check-icon.svg';
+
 
 const About = () => {
 
@@ -11,17 +15,17 @@ const About = () => {
         <div className="about-title-border"></div>
         <div className="about-container">
           <div>
-            <h3 className="about-title">{asset.MAIN.ABOUT.experience.title}</h3>
+            <h3 className="about-title"><span className="icons"><img src={person} alt="person icon"/></span>{asset.MAIN.ABOUT.experience.title}</h3>
             <p className="about-description">{asset.MAIN.ABOUT.experience.description}</p>
           </div>
           <div className="experience-border"></div>
           <div>
-            <h3 className="about-title">{asset.MAIN.ABOUT.personality.title}</h3>
+            <h3 className="about-title"><span className="icons" ><img src={smile} alt="smile icon"/></span>{asset.MAIN.ABOUT.personality.title}</h3>
             <p className="about-description">{asset.MAIN.ABOUT.personality.description}</p>
           </div>
           <div className="personality-border"></div>
           <div>
-            <h3 className="about-title">{asset.MAIN.ABOUT.goals.title}</h3>
+            <h3 className="about-title"><span  className="icons"><img src={check} alt="check icon"/></span>{asset.MAIN.ABOUT.goals.title}</h3>
             <p className="about-description">{asset.MAIN.ABOUT.goals.description}</p>
           </div>
         </div>
